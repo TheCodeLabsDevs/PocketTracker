c1bd3e8f1eb744d02a966e3ee8005d61ab5c0345
Robert Goldmann <deadlocker@gmx.de>
2021-01-19 22:45:55 +0100

    #1 - added user entity and repo

---
+++ src/main/java/de/thecodelabs/pockettracker/user/UserRepository.java
@@ -0,0 +1,9 @@
+package de.thecodelabs.pockettracker.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer>
+{
+}
