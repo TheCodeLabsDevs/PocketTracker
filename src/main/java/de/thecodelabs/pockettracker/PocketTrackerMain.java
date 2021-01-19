9ee57acca7040117ec2100d5646c09786bcc9156
Robert Goldmann <deadlocker@gmx.de>
2021-01-19 21:15:46 +0100

    added spring boot basi setup

---
+++ src/main/java/de/thecodelabs/pockettracker/PocketTrackerMain.java
@@ -0,0 +1,13 @@
+package de.thecodelabs.pockettracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketTrackerMain
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(PocketTrackerMain.class, args);
+	}
+}
