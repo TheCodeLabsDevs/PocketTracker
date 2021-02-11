3ae3a597c15600d24acea2d464ee02c6e363b4b8
tobias <thinkdifferent055@gmail.com>
2021-02-11 20:12:49 +0100

    #2 - Add dynamic version to openapi

--- src/main/java/de/thecodelabs/pockettracker/PocketTrackerMain.java
+++ src/main/java/de/thecodelabs/pockettracker/PocketTrackerMain.java
@@ -1,23 +1,22 @@
 package de.thecodelabs.pockettracker;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+import java.util.List;
 
 @SpringBootApplication
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-@OpenAPIDefinition(
-		info = @Info(title = "PocketTracker", version = "1.0.0"),
-		security = {
-				@SecurityRequirement(name = "token")
-		}
-)
 @SecurityScheme(
 		name = "token",
 		type = SecuritySchemeType.APIKEY,
@@ -29,5 +28,14 @@ public class PocketTrackerMain
 	public static void main(String[] args)
 	{
 		SpringApplication.run(PocketTrackerMain.class, args);
+	}
+
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${pockettracker.version}") String appVersion)
+	{
+		return new OpenAPI()
+				.components(new Components())
+				.security(List.of(new SecurityRequirement().addList("token")))
+				.info(new Info().title("PocketTracker").version(appVersion));
 	}
 }
