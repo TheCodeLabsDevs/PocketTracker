2b1274b58fbf85bf155afbafca13d018703ae05b
tobias <thinkdifferent055@gmail.com>
2021-02-11 21:53:11 +0100

    Extract user settings controller methods into new class

--- src/main/java/de/thecodelabs/pockettracker/user/controller/UserController.java
+++ src/main/java/de/thecodelabs/pockettracker/user/controller/UserController.java
@@ -2,42 +2,31 @@ package de.thecodelabs.pockettracker.user.controller;
 
 import de.thecodelabs.pockettracker.episode.model.Episode;
 import de.thecodelabs.pockettracker.episode.repository.EpisodeRepository;
-import de.thecodelabs.pockettracker.exceptions.NotFoundException;
 import de.thecodelabs.pockettracker.season.model.Season;
 import de.thecodelabs.pockettracker.season.reposiroty.SeasonRepository;
 import de.thecodelabs.pockettracker.show.ShowRepository;
 import de.thecodelabs.pockettracker.show.ShowService;
 import de.thecodelabs.pockettracker.show.model.Show;
 import de.thecodelabs.pockettracker.show.model.ShowSortOption;
-import de.thecodelabs.pockettracker.user.PasswordValidationException;
 import de.thecodelabs.pockettracker.user.model.User;
 import de.thecodelabs.pockettracker.user.model.WatchedEpisode;
-import de.thecodelabs.pockettracker.user.model.authentication.GitlabAuthentication;
-import de.thecodelabs.pockettracker.user.model.authentication.UserAuthentication;
-import de.thecodelabs.pockettracker.user.service.UserAuthenticationService;
 import de.thecodelabs.pockettracker.user.service.UserService;
 import de.thecodelabs.pockettracker.utils.BootstrapColor;
 import de.thecodelabs.pockettracker.utils.WebRequestUtils;
 import de.thecodelabs.pockettracker.utils.toast.Toast;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.WebRequest;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
 import java.text.MessageFormat;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import static de.thecodelabs.pockettracker.MainController.PARAMETER_NAME_IS_USER_SPECIFIC_VIEW;
 import static de.thecodelabs.pockettracker.MainController.PARAMETER_NAME_SEARCH_TEXT;
@@ -46,10 +35,7 @@ import static de.thecodelabs.pockettracker.MainController.PARAMETER_NAME_SEARCH_
 @RequestMapping("/user")
 public class UserController
 {
-	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
-
 	private final UserService userService;
-	private final UserAuthenticationService authenticationService;
 
 	private final ShowRepository showRepository;
 	private final ShowService showService;
@@ -57,85 +43,13 @@ public class UserController
 	private final EpisodeRepository episodeRepository;
 
 	@Autowired
-	public UserController(UserService userService, UserAuthenticationService authenticationService, ShowRepository showRepository, ShowService showService, SeasonRepository seasonRepository, EpisodeRepository episodeRepository)
+	public UserController(UserService userService, ShowRepository showRepository, ShowService showService, SeasonRepository seasonRepository, EpisodeRepository episodeRepository)
 	{
 		this.userService = userService;
-		this.authenticationService = authenticationService;
 		this.showRepository = showRepository;
 		this.showService = showService;
 		this.seasonRepository = seasonRepository;
 		this.episodeRepository = episodeRepository;
-	}
-
-	@GetMapping("/settings")
-	public String settingsView(HttpServletRequest request, Model model)
-	{
-		final Optional<User> userOptional = userService.getCurrentUserOptional();
-		if(userOptional.isEmpty())
-		{
-			try
-			{
-				request.logout();
-			}
-			catch(ServletException e)
-			{
-				logger.error("Cannot logout user", e);
-			}
-			return "redirect:/";
-		}
-
-		final User user = userOptional.get();
-		model.addAttribute("user", new UserForm(user));
-		model.addAttribute("authentications", user.getAuthentications().stream().sorted(Comparator.comparing(UserAuthentication::getType)).collect(Collectors.toList()));
-		model.addAttribute("isGitlabConnected", user.getAuthentications().stream().anyMatch(provider -> provider instanceof GitlabAuthentication));
-
-		return "users/edit";
-	}
-
-	@PostMapping("/settings")
-	public String settingsSubmit(WebRequest request, @ModelAttribute("user") UserForm userForm)
-	{
-		try
-		{
-			userService.editUser(userService.getCurrentUser(), userForm);
-		}
-		catch(PasswordValidationException e)
-		{
-			WebRequestUtils.putToast(request, new Toast("toast.validatePassword", BootstrapColor.DANGER));
-			return "redirect:/user/settings";
-		}
-
-		WebRequestUtils.putToast(request, new Toast("toast.saved", BootstrapColor.SUCCESS));
-		return "redirect:/user/settings";
-	}
-
-	@PostMapping("/settings/oauth/{provider}")
-	public String connectOauth(WebRequest request, @PathVariable String provider,
-							   @ModelAttribute("oauth") @Validated UserOauthForm oauthForm, BindingResult result)
-	{
-		if(result.hasErrors())
-		{
-			WebRequestUtils.putToast(request, new Toast("authentication.provider.gitlab.missingUsername", BootstrapColor.DANGER));
-			return "redirect:/user/settings";
-		}
-
-		Optional<User> userOptional = userService.getUser(SecurityContextHolder.getContext().getAuthentication());
-		if(userOptional.isEmpty())
-		{
-			throw new NotFoundException("User not found");
-		}
-
-		final User user = userOptional.get();
-		userService.addGitlabAuthentication(user, oauthForm.getUsername());
-
-		return "redirect:/oauth2/authorization/" + provider;
-	}
-
-	@PostMapping("/settings/provider/{id}/delete")
-	public String deleteProvider(@PathVariable Integer id)
-	{
-		authenticationService.deleteAuthenticationProvider(userService.getCurrentUser(), id);
-		return "redirect:/user/settings";
 	}
 
 	@GetMapping("/shows")
---
+++ src/main/java/de/thecodelabs/pockettracker/user/controller/UserSettingsController.java
@@ -0,0 +1,116 @@
+package de.thecodelabs.pockettracker.user.controller;
+
+import de.thecodelabs.pockettracker.exceptions.NotFoundException;
+import de.thecodelabs.pockettracker.user.PasswordValidationException;
+import de.thecodelabs.pockettracker.user.model.User;
+import de.thecodelabs.pockettracker.user.model.authentication.GitlabAuthentication;
+import de.thecodelabs.pockettracker.user.model.authentication.UserAuthentication;
+import de.thecodelabs.pockettracker.user.service.UserAuthenticationService;
+import de.thecodelabs.pockettracker.user.service.UserService;
+import de.thecodelabs.pockettracker.utils.BootstrapColor;
+import de.thecodelabs.pockettracker.utils.WebRequestUtils;
+import de.thecodelabs.pockettracker.utils.toast.Toast;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/user/settings")
+public class UserSettingsController
+{
+	private static final Logger logger = LoggerFactory.getLogger(UserSettingsController.class);
+
+	private final UserService userService;
+	private final UserAuthenticationService authenticationService;
+
+	@Autowired
+	public UserSettingsController(UserService userService, UserAuthenticationService authenticationService)
+	{
+		this.userService = userService;
+		this.authenticationService = authenticationService;
+	}
+
+	@GetMapping
+	public String settingsView(HttpServletRequest request, Model model)
+	{
+		final Optional<User> userOptional = userService.getCurrentUserOptional();
+		if(userOptional.isEmpty())
+		{
+			try
+			{
+				request.logout();
+			}
+			catch(ServletException e)
+			{
+				logger.error("Cannot logout user", e);
+			}
+			return "redirect:/";
+		}
+
+		final User user = userOptional.get();
+		model.addAttribute("user", new UserForm(user));
+		model.addAttribute("authentications", user.getAuthentications().stream().sorted(Comparator.comparing(UserAuthentication::getType)).collect(Collectors.toList()));
+		model.addAttribute("isGitlabConnected", user.getAuthentications().stream().anyMatch(provider -> provider instanceof GitlabAuthentication));
+
+		return "users/edit";
+	}
+
+	@PostMapping
+	public String settingsSubmit(WebRequest request, @ModelAttribute("user") UserForm userForm)
+	{
+		try
+		{
+			userService.editUser(userService.getCurrentUser(), userForm);
+		}
+		catch(PasswordValidationException e)
+		{
+			WebRequestUtils.putToast(request, new Toast("toast.validatePassword", BootstrapColor.DANGER));
+			return "redirect:/user/settings";
+		}
+
+		WebRequestUtils.putToast(request, new Toast("toast.saved", BootstrapColor.SUCCESS));
+		return "redirect:/user/settings";
+	}
+
+	@PostMapping("/oauth/{provider}")
+	public String connectOauth(WebRequest request, @PathVariable String provider,
+							   @ModelAttribute("oauth") @Validated UserOauthForm oauthForm, BindingResult result)
+	{
+		if(result.hasErrors())
+		{
+			WebRequestUtils.putToast(request, new Toast("authentication.provider.gitlab.missingUsername", BootstrapColor.DANGER));
+			return "redirect:/user/settings";
+		}
+
+		Optional<User> userOptional = userService.getUser(SecurityContextHolder.getContext().getAuthentication());
+		if(userOptional.isEmpty())
+		{
+			throw new NotFoundException("User not found");
+		}
+
+		final User user = userOptional.get();
+		userService.addGitlabAuthentication(user, oauthForm.getUsername());
+
+		return "redirect:/oauth2/authorization/" + provider;
+	}
+
+	@PostMapping("/provider/{id}/delete")
+	public String deleteProvider(@PathVariable Integer id)
+	{
+		authenticationService.deleteAuthenticationProvider(userService.getCurrentUser(), id);
+		return "redirect:/user/settings";
+	}
+}
