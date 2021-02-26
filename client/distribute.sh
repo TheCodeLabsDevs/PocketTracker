1ba60242fffc6480bebca25d0f7952e4a2c7fa0f
tobias <thinkdifferent055@gmail.com>
2021-02-26 22:26:32 +0100

    Add interpreter to client distribute script

--- client/distribute.sh
+++ client/distribute.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/5.0.1/openapi-generator-cli-5.0.1.jar -O openapi-generator-cli.jar
 java -jar openapi-generator-cli.jar generate \
   -i api-docs.json \
