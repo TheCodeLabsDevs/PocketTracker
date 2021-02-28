3e6a75a0b9788e9544ad2e53c9da00b97d34e95e
Robert Goldmann <deadlocker@gmx.de>
2021-02-28 17:44:03 +0100

    cleanup imports

--- src/main/java/de/thecodelabs/pockettracker/backup/BatchEditController.java
+++ src/main/java/de/thecodelabs/pockettracker/backup/BatchEditController.java
@@ -1,7 +1,5 @@
 package de.thecodelabs.pockettracker.backup;
 
-import de.thecodelabs.pockettracker.backup.configuration.BackupConfigurationProperties;
-import de.thecodelabs.pockettracker.backup.service.BackupService;
 import de.thecodelabs.pockettracker.episode.model.Episode;
 import de.thecodelabs.pockettracker.show.ShowService;
 import de.thecodelabs.pockettracker.show.model.Show;
@@ -11,24 +9,15 @@ import de.thecodelabs.pockettracker.utils.toast.Toast;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.net.URLDecoder;
-import java.nio.charset.Charset;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.sql.SQLException;
 import java.text.MessageFormat;
 import java.util.List;
 import java.util.Optional;
