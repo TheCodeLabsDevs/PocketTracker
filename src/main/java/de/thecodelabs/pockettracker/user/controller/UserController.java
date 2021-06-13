2992daac29fc7bd386378fc924999260e67d0782
tobias <thinkdifferent055@gmail.com>
2021-06-13 20:40:51 +0200

    Only show "show add" button if show is not already added

--- src/main/java/de/thecodelabs/pockettracker/user/controller/UserController.java
+++ src/main/java/de/thecodelabs/pockettracker/user/controller/UserController.java
@@ -26,6 +26,7 @@ import org.springframework.web.context.request.WebRequest;
 import java.text.MessageFormat;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static de.thecodelabs.pockettracker.MainController.PARAMETER_NAME_IS_USER_SPECIFIC_VIEW;
@@ -89,7 +90,7 @@ public class UserController
 		model.addAttribute("currentSortOption", sortOption);
 
 		model.addAttribute("currentPage", "Meine Serien");
-		model.addAttribute("userShows", user.getShows());
+		model.addAttribute("userShows", user.getShows().stream().map(AddedShow::getShow).collect(Collectors.toList()));
 		model.addAttribute(PARAMETER_NAME_IS_USER_SPECIFIC_VIEW, true);
 
 		return "index";
